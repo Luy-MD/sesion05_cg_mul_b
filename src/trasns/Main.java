fc7946ba4d67424f2838005ff1f97b7b463bfc75
Luy-MD <est.luz.madrigal@unimilitar.edu.co>
2021-08-26 16:14:59 -0500

    Signed-off-by: Luz Madrigal <est.luz.madrigal@unimilitar.edu.co>

---
+++ src/trasns/Main.java
@@ -0,0 +1,69 @@
+package trasns;
+
+import java.util.Scanner;
+
+public class Main
+{
+	static Scanner entrada = new Scanner(System.in);
+	public static void main(String[] args) {
+		// Defina y asigne dos objetos, un Cadrado y una Circunferencia
+		// Solicite al usuario ingresar el valor de un vector de traslación 
+		// Aplique el vector de traslación anterior a cada uno de los objetos referidos
+		// Muestre las posiciones originales y finales de cada uno de los objetos referidos
+		
+		System.out.println("Ingrese un vector de translacion");
+		System.out.println("x: ");
+		int x = entrada.nextInt();
+		System.out.println("y: ");
+		int y = entrada.nextInt();
+	}
+}
+
+class Coord {
+    private int x, y;
+    
+    public void setX(int x) {
+    	this.x = x;
+    }
+    
+    public void setY(int y) {
+    	this.y = y;
+    }
+    
+    public int getX() {
+    	return x;
+    }   
+    
+    public int getY() {
+    	return y;
+    }
+
+class Cuadrado {
+    private Coord c;
+    private int lado;
+    public Cuadrado(Coord c, int lado) {
+        this.c = c;
+        this.lado = lado;
+    }
+    public void Traslacion(Coord t) {  // Complete los parámetros requeridos
+     
+    	c.setX(t.getX()+c.getX());
+    	c.setY(t.getY()+c.getY());
+    	
+    }
+    public void Escalado(int s) {  // Complete los parámetros requeridos
+        
+    	lado = (int)(lado*(1+s/100.0f));
+    }
+    public void Rotacion() {  // Complete los parámetros requeridos
+        
+    }
+}
+
+class Circunferencia {
+    Coord c;
+    float radio;
+    // Implementar los métodos Traslacion, Escalado y Rotacion para ésta clase
+}
+
+}
